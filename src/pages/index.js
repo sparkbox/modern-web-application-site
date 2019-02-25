d2284936e2925d88d448ce1a95551af524941af3
Melissa Thompson <mthompson@heysparkbox.com>
2019-02-25 14:36:24 -0500

    minor content changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,36 +7,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`modern web application`, `tutorial`]} />
     <section>
-      <h1>Purpose</h1>
-      <hr />
-      <p>
-        Web applications are becoming more and more complex as the industry matures.
-        We’re seeing a large push towards performance, efficiency, and resiliency, which can sometimes come at the cost of developer satisfaction.
-      </p>
-      <p>
-        First, we'll define what a "modern web application" means, then we’ll go through the process of getting a sample web application up and running using the following:
-      </p>
-      <ul>
-        <li>NodeJS on Express</li>
-        <li>Ruby on Rails</li>
-        <li>PHP on Laravel</li>
-        <li>Python on Django</li>
-      </ul>
-      <p>
-        Once you've completed these tutorials, you’ll begin to feel a sense of ownership over your projects and feel empowered to implement these practices on new and existing code bases.
-      </p>
-    </section>
-    <section>
       <h1>What is a Modern Web Application</h1>
       <hr />
-      <p>The definition of a modern development environment is constantly changing. We’re moving towards a more modular, performant, and resilient architectures when crafting our web applications. Let’s break these three higher-level goals down to create a list of criteria that each of our environments must maintain to be considered “modern”.</p>
+      <p>The definition of a modern development environment is constantly changing. We’re moving towards more modular, performant, and resilient architectures when crafting our web applications. Let’s break these three higher-level goals down to create a list of criteria that each of our environments can maintain to be considered “modern”.</p>
 
       <dl>
         <dt>Modular</dt>
         <dd>
           <p>To keep our applications <a href="https://en.wikipedia.org/wiki/Don%27t_repeat_yourself">DRY</a>, we are seeing a trend towards making our code more modular. This involves breaking our HTML into reusable templates that serve smaller purposes. We can see examples of this in patterns such as <a href="http://bradfrost.com/blog/post/atomic-web-design/">Atomic Design</a>, so we’ll define our first web application environment requirement as the ability to create and maintain HTML templates that can be reused on our site.</p>
           <p>This concept applies just as much to CSS as it does to HTML. We’ll want to write CSS that matches up one-to-one with our HTML templates, which is going to be easiest to do with the help of a pre-processor. In our case, our next requirement for our modern environment will be to configure and use <a href="https://sass-lang.com/">SASS</a> as a CSS pre-processor.</p>
-          <p>When we’re looking at how to apply this concept to our Javascript, we’ll want to enable ourselves to break our logic up by using the `export` and `import` syntax not yet available to us in normal Javascript. This means we’ll want to take advantage of some newer Javascript techniques that will bring us to our third requirement: configuring the ability to transpile newer Javascript using <a href="https://babeljs.io/">Babel</a>.</p>
+          <p>When we’re looking at how to apply this concept to our Javascript, we’ll want to enable ourselves to break our logic up by using the <code>export</code> and <code>import</code> syntax not yet available to us in normal Javascript. This means we’ll want to take advantage of some newer Javascript techniques that will bring us to our third requirement: configuring the ability to transpile newer Javascript using <a href="https://babeljs.io/">Babel</a>.</p>
         </dd>
         <dt>Performant</dt>
         <dd>
--- src/pages/workshop.js
+++ src/pages/workshop.js
@@ -10,13 +10,23 @@ const WorkshopPage = () => (
     <p>The workshop for frontend developers who want to learn today’s best practices in application development along with an introduction to many of today’s top-used development environments.</p>
     <hr />
 
-    <q>Let’s face it, sometimes setting up a new application seems daunting. <br />It doesn’t have to be. We’ll show you what goes on behind the scenes as we navigate different languages and frameworks and get comfortable with the idea of a brand new codebase.</q>
+    <p>
+      <q>Let’s face it, sometimes setting up a new application seems daunting. It doesn’t have to be. We’ll show you what goes on behind the scenes as we navigate different languages and frameworks and get comfortable with the idea of a brand new codebase.</q>
+    </p>
 
     <h2>Description</h2>
     <p>Web applications are becoming more and more complex as the industry matures. We’re seeing a large push towards performance, efficiency, and resiliency, which can sometimes come at the cost of developer satisfaction. We’ll explain the core concepts behind a modern web application as we demonstrate how to apply each one to our industry’s top-used languages and frameworks in a way that minimizes complexity and makes development easy.</p>
 
     <h2>Our Promise To You</h2>
     <p>Together, we’ll navigate different languages and frameworks while we learn the core concepts behind a modern web application. You’ll begin to feel a sense of ownership over your projects and feel empowered to implement these practices on new and existing code bases.</p>
+
+    <h2>Workshop Languages and Frameworks</h2>
+    <ul>
+      <li>NodeJS on Express</li>
+      <li>Ruby on Rails</li>
+      <li>PHP on Laravel</li>
+      <li>Python on Django</li>
+    </ul>
 
     <h2>Workshop Topics</h2>
     <ul>
